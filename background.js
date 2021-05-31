704bfcc2f9d438fcf454ed31187d57f135435e33
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-06-01 00:51:18 +0200

    fix: menu entry title refactor: use es6 style function

--- background.js
+++ background.js
@@ -2,14 +2,15 @@
 //const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
+const menu_title = "Open new tab beside"
 
 browser.menus.create({
   id: extname,
-  title: "Open new tab next to current",
+  title: menu_title,
   contexts: ["tab"]
 });
 
-browser.menus.onClicked.addListener(function(info, tab) {
+browser.menus.onClicked.addListener((info, tab) => {
 	if (info.menuItemId == extname) {
 		browser.tabs.create({
 			index: (tab.index + 1)
