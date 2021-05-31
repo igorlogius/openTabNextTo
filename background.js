13adc625882fc84085ebb0a12a1e9e2b24ba7c40
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-05-31 21:11:38 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,19 @@
+
+//const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+
+browser.menus.create({
+  id: extname,
+  title: "Open new Tab next to current",
+  contexts: ["tab"]
+});
+
+browser.menus.onClicked.addListener(function(info, tab) {
+	if (info.menuItemId == extname) {
+		browser.tabs.create({
+			index: (tab.index + 1)
+		});
+	}
+});
+
